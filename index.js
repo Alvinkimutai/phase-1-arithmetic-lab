8331bb41ac4fdf8b94f623fc5da960401175246d
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-06-28 05:53:11 +0300

    arithmetic test

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+//Create a variable called multiply set to an equation that will multiply the variables num1 and num2; 
+//the result of the multiplication should be 62
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1 * num2);
+console.log(multiply);
+
+//Create a variable called random that will generate a random integer greater than 0.
+const random =Math.floor(Math.random()*10) + 1;
+console.log(random);
+
+//Create a variable called mod set to an equation that will calculate 
+//the remainder of dividing variable num3 by num4; the remainder should be 4
+let num3 = 64;
+let num4 = 5;
+const mod = num3%num4;
+console.log(mod);
+
+//Create a variable called max that finds the highest number in a set; the value returned should be 20
+let max = Math.max(5,10,15,20);
+console.log(max);
